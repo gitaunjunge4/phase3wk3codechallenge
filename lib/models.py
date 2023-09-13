c87e4571123c604c7b2de15e2909da2dc27eeab9
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-09-13 13:05:00 +0300

    added the restaurant and customer model

--- lib/models.py
+++ lib/models.py
@@ -13,5 +13,26 @@ engine = create_engine('sqlite:///restraurants.db')
 
 Base = declarative_base(metadata=metadata)
 
-class 
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
 
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), nullable=False)
+    price = Column(Float(), nullable=False)
+
+    def __repr__(self):
+        return f"Restaurant ID: {self.id}, " \
+            + f"Name: {self.name}, " \
+            + f"Price: {self.price}"
+    
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String(), nullable=False)
+    last_name = Column(String(), nullable=False)
+
+    def __repr__(self):
+        return f"Restaurant ID: {self.id}, " \
+            + f"First Name: {self.first_name}, " \
+            + f"Last Name: {self.last_name}"
